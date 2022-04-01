3b18f335465d166056ec2c61e20429558a9166af
Ritesh Kumar <rit23kumar@gmail.com>
2022-04-02 00:56:16 +0530

    first look

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// TOGGLE FUNCTION
+function hideMenu() {
+  document.getElementById('navigation').style.display = 'none';
+  document.getElementById('show-menu').style.display = 'block';
+  document.getElementById('hide-menu').style.display = 'none';
+}
+function showMenu() {
+  document.getElementById('navigation').style.display = 'block';
+  document.getElementById('show-menu').style.display = 'none';
+  document.getElementById('hide-menu').style.display = 'block';
+}
